be9ccc1d41a165554fb279be445c9c77abd8d254
felipe <felipe.rosa@sysmap.com.br>
2019-01-28 22:53:00 -0200

    Novas alteracoes

---
+++ src/main/java/com/felipe/uolChallenge/auxiliares/GetLocationByIp.java
@@ -0,0 +1,5 @@
+package com.felipe.uolChallenge.auxiliares;
+
+public class GetLocationByIp {
+
+}
---
+++ src/main/java/com/felipe/uolChallenge/domain/Data.java
@@ -0,0 +1,37 @@
+package com.felipe.uolChallenge.domain;
+
+public class Data {
+	
+	private String ipv4;
+	private String subdivision_1_name;
+	private String latitude;
+	private String longitude;
+	
+	
+	public String getIpv4() {
+		return ipv4;
+	}
+	public void setIpv4(String ipv4) {
+		this.ipv4 = ipv4;
+	}
+	public String getSubdivision_1_name() {
+		return subdivision_1_name;
+	}
+	public void setSubdivision_1_name(String subdivision_1_name) {
+		this.subdivision_1_name = subdivision_1_name;
+	}
+	public String getLatitude() {
+		return latitude;
+	}
+	public void setLatitude(String latitude) {
+		this.latitude = latitude;
+	}
+	public String getLongitude() {
+		return longitude;
+	}
+	public void setLongitude(String longitude) {
+		this.longitude = longitude;
+	}
+	
+	
+}
